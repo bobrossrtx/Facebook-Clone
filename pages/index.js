a98ccc87a98a851f2a04f92dc0e46d410f5ffe3f
Owen Boreham <bobrossrtx@gmail.com>
2021-07-06 15:39:47 +0100

    Added header, Sidebar & Feed to index

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,37 @@
-import Head from 'next/head'
+import { getSession } from 'next-auth/client';
+import Head from 'next/head';
+import Feed from '../components/Feed';
+import Header from '../components/Header';
+import Login from '../components/Login';
+import Sidebar from '../components/Sidebar';
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />;
+  
   return (
-    <div>
+    <div className="h-screen bg-gray-100 overflow-hidden">
       <Head>
         <title>Facebook</title>
       </Head>
 
-      {/* Header */}
+      <Header session={session} />
 
-      <main>
-        {/* Sidebar */}
-        {/* Feed */}
+      <main className="flex">
+        <Sidebar session={session} />
+        <Feed />
         {/* Widgets */}
       </main>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps(context) {
+  // Get the username
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session
+    }
+  }
 }
